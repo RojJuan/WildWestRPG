cf0ccdffe22385279c4b63f8c224914d4c810ae1
Juan R <Juankingboo@proton.me>
2024-12-12 16:52:39 -0500

    added a main menu

--- scenes/mainmenu.gd
+++ scenes/mainmenu.gd
@@ -6,8 +6,8 @@ func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/someplace_somewhere.tscn")
 	
 
-func _on_options_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/optionsmenu.tscn")
+#func _on_options_pressed() -> void:
+#	get_tree().change_scene_to_file("res://scenes/optionsmenu.tscn")
 
 
 
