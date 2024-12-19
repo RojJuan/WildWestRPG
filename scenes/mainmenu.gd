2db06545edca8a751dd64bf5f1bf3fe0e1c35f22
Juan R <Juankingboo@proton.me>
2024-12-19 16:55:10 -0500

    added cool menu shit

--- scenes/mainmenu.gd
+++ scenes/mainmenu.gd
@@ -6,10 +6,13 @@ func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/someplace_somewhere.tscn")
 	
 
-#func _on_options_pressed() -> void:
-#	get_tree().change_scene_to_file("res://scenes/optionsmenu.tscn")
+
 
 
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/optionsmenu.tscn")
--- scenes/optionsmenu.gd
+++ scenes/optionsmenu.gd
@@ -2,6 +2,18 @@ extends Control
 
 
 
-
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/mainmenu.tscn")
+
+
+func _on_h_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(0, linear_to_db(value))
+
+
+func _on_fullscreen_toggled(toggled_on: bool) -> void:
+	if toggled_on == true:
+		$VBoxContainer/fullscreen.text = "Fullscreen: on"
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		$VBoxContainer/fullscreen.text = "Fullscreen: off"
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
